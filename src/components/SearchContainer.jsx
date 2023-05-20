348487bd38f653597e8ba8177273994a6cdbfa3d
RushikeshB7 <rushikesh@salesken.ai>
2023-05-20 19:01:35 +0530

    select options putting static

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -8,8 +8,8 @@ const SearchContainer = () => {
   const [status, setStatus] = useState("");
   const [capsuleSerial, setCapsuleSerial] = useState("");
   const data = useSelector((store) => store.reducer);
-  const [capsuleSerialOpt,setCapsuleSerialOpt]=useState([])
-  const [capsuleStatusOpt,setCapsuleStatusOpt]=useState([])
+  const [capsuleSerialOpt, setCapsuleSerialOpt] = useState([]);
+  const [capsuleStatusOpt, setCapsuleStatusOpt] = useState([]);
   const [total, setTotal] = useState(data.length);
 
   useEffect(() => {
@@ -42,7 +42,7 @@ const SearchContainer = () => {
 
           {/* Dropdowns */}
           <div className="sm:col-span-6 lg:col-span-2">
-          <input
+            <input
               type="text"
               name="search"
               placeholder="Search by Capsule Serial"
@@ -56,25 +56,28 @@ const SearchContainer = () => {
 
           <div className="sm:col-span-6 lg:col-span-1">
             <select
-              name="status"  onChange={(e) => {
+              name="status"
+              onChange={(e) => {
                 setStatus(e.target.value);
               }}
               className="w-full bg-gray cursor-pointer rounded-md border-2 py-2 px-4 text-white border-white  shadow-sm focus:ring-2 focus:ring-slate-50 sm:text-sm"
             >
               <option value="">Select Status</option>
-              {capsuleStatusOpt && capsuleStatusOpt.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
+              <option value="retired">Retired</option>
+              <option value="active">Active</option>
+              <option value="destroyed">Destroyed</option>
+              <option value="unknown">Unknown</option>
             </select>
           </div>
 
           {/* Apply Filter Button */}
           <div className="sm:col-span-6 lg:col-span-1">
-            <button className="w-full border border-white text-white py-2 px-4 rounded-md shadow-sm sm:text-sm"  onClick={(e) => {
-          handelFilters();
-        }}>
+            <button
+              className="w-full border border-white text-white py-2 px-4 rounded-md shadow-sm sm:text-sm"
+              onClick={(e) => {
+                handelFilters();
+              }}
+            >
               Apply Filter
             </button>
           </div>
